5abed50756c6cce14f3348724fb504f2b6e1aa3d
nyasharonaldo <nyasharonaldo@gmail.com>
2021-11-20 15:30:28 +0000

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,21 +17,10 @@ module.exports = {
         trackingIds: [
           "G-899533TZPF", // Google Analytics / GA
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: true,
-        //   cookie_expires: 0,
-        // },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: true,
-          // // Setting this parameter is also optional
-          // respectDNT: true,
-          // // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+          anonymize_ip: true
         },
       },
     },
