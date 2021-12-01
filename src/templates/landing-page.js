19cb2ef66a5994875d9606712d9c64ebc063c79a
nyasharonaldo <nyasharonaldo@gmail.com>
2021-12-01 19:44:55 +0000

    grammar changes

--- src/templates/landing-page.js
+++ src/templates/landing-page.js
@@ -75,7 +75,10 @@ const Landing = ({ data }) => {
         </div>
       </div>
 
-      <div className="section-container border-section-container" id="applications">
+      <div
+        className="section-container border-section-container"
+        id="applications"
+      >
         <div className="container">
           <div className="row">
             <div className="col-md-12 section-container-spacer">
@@ -92,31 +95,46 @@ const Landing = ({ data }) => {
               </div>
               <div>
                 <p>
-                  The product is frost resistant and is very well drained to
-                  produce a cushioned non-slip surface that is safe for both the
-                  horse and the rider. Rubber chippings are safe for equestrian
-                  use in turnout areas as horses tend not to eat it and the
-                  softer nature of rubber chip is kinder on the hooves.The
-                  product is frost resistant and is very well drained to produce
-                  a cushioned non-slip surface that is safe for both the horse
-                  and the rider. Rubber chippings are safe for equestrian use in
-                  turnout areas as horses tend not to eat it and the softer
-                  nature of rubber chip is kinder on the hooves.
+                  If you use our rubber chippings the surface of your arena
+                  wouldn't lose its integrity through biodegradation, like wood
+                  chip would. It won’t become muddy or slippery in wintery or
+                  wet conditions like grass. It’s also less prone to freezing,
+                  and it is springy, making it safer to fall on.
                 </p>
+                <h5>Why ours?</h5>
+                <ul>
+                  <li>
+                    The equestrian rubber resists frost to around -5 degrees
+                    giving a truly year-round riding experience
+                  </li>
+                  <li>
+                    It Minimises leg concussion risk by offering a level of
+                    protection for feet and tendons
+                  </li>
+                  <li>
+                    Very little maintenance as or annual replenishment is
+                    required as the rubber does not decompose, break down or
+                    blow away
+                  </li>
+                  <li>Our chipping aids surface drainage</li>
+                </ul>
+
+                <h5>Construction</h5>
                 <p>
-                  <h5>Construction</h5> Equstrian arena require a 100mm silica
-                  sand layed upon a drainage channels, topped with 50mm of
-                  Rubber chipping, allowing you to exercise your equine
-                  companion in all weathers.
+                  To make up the arena surface, tyres are processed into small
+                  rubber chippings. 100mm of Silica Sand is laid down, followed
+                  by 50mm of Rubber Chippings which form a soft, grippy surface.
+                  This is laid over drainage channels, allowing you to exercise
+                  your equine companion in all weathers.
                 </p>
-                <p>
-                  <h5>How Much Will I Need?</h5> To achieve the recommended
-                  50mm depth you will require 25kg of rubber chippings per
-                  square metre.
-                </p>
+                <h5>Where can I buy Silica Sand?</h5>
                 <p>
                   We offer competitive rates for both Rubber Chippings and
-                  Silica Sand.
+                  Silica Sand. Email{" "}
+                  <a href="mailto:info@southernchippings.co.uk">
+                    info@southernchippings.co.uk
+                  </a>{" "}
+                  for more information.
                 </p>
               </div>
             </div>
@@ -136,17 +154,19 @@ const Landing = ({ data }) => {
                   their best and provide a clean dust free environment for your
                   chickens.
                 </p>
+                <h5>Construction</h5>
                 <p>
-                  <h5>Construction</h5> Lay a basic membrane down and simply
-                  place the rubber chippings on top 50mm will do perfect. Do not
-                  apply to concrete bases this will result in drainage problems.
+                  Lay a basic membrane down and simply place the rubber
+                  chippings on top 50mm will do perfect. Do not apply to
+                  concrete bases this will result in drainage problems.
                 </p>
+                <h5>How Much Will I Need?</h5>
                 <p>
-                  <h5>How Much Will I Need?</h5> To achieve the recommended
-                  50mm depth you will require 25kg of rubber chippings per
-                  square metre.
+                  To achieve the recommended 50mm depth you will require 25kg of
+                  rubber chippings per square metre.
                 </p>
-                <p><h5>Maintenance</h5> Rince regulerly</p>
+                <h5>Maintenance</h5>
+                <p> Rince regularly</p>
               </div>
             </div>
             <div className="col-md-12 section-container-spacer">
@@ -159,25 +179,33 @@ const Landing = ({ data }) => {
               <div>
                 <p>
                   Using rubber mulch in your landscaping is the healthier choice
-                  for plants then such alternitives as wood chippings because it
+                  for plants then such alternatives as wood chippings because it
                   allows for water and nutrients to reach plant roots that other
                   mulches retain, it doesn't attract pests that can often damage
                   plants, and helps prevent weeds leading to less chemical use
                   for control.
                 </p>
+                <h5>Construction</h5>
                 <p>
-                  <h5>Construction</h5> Weed the garden thorougherly and do not
-                  place heavy amounts next to tree stumps or plant stems as they
-                  need room to breath. Soak the mulch in a bucket for 24Hours to
-                  ensure there are no oils left on material.
+                  Weed the garden thoroughly and do not place heavy amounts next
+                  to tree stumps or plant stems as they need room to breathe.
+                  Soak the mulch in a bucket for 24Hours to ensure there are no
+                  oils left on material.
                 </p>
+                <h5>How Much Will I Need?</h5>
                 <p>
-                  <h5>How Much Will I Need?</h5> Up to you, depends on your
-                  garden and how you wish for it to look
+                  {" "}
+                  Up to you, depends on your garden and how you wish for it to
+                  look. If you'd like some help in understanding how much will
+                  be required for your use case don't hestitate to reach out to
+                  us at{" "}
+                  <a href="mailto:info@southernchippings.co.uk">
+                    info@southernchippings.co.uk
+                  </a>.{" "}
                 </p>
                 <p>
                   We offer local delivery for any size delivery and national
-                  delivery for 10+ Tones
+                  delivery for 10+ Tones.
                 </p>
               </div>
             </div>
@@ -215,11 +243,14 @@ const Landing = ({ data }) => {
               <ul className="features">
                 <li>
                   <h3>
-                    Rubber Tree’s are grown in hot wet climates e.g. South
+                    Rubber Trees are grown in hot wet climates e.g. South
                     America, Rubber processing technically begins at the point
                     of harvesting, when tappers take latex sap from rubber
-                    trees. Next, the latex is filtered and packaged into drums,
-                    then sent to be made into sheets
+                    trees.
+                  </h3>
+                  <h3>
+                    Next, the latex is filtered and packaged into drums, then
+                    sent to be made into sheets.
                   </h3>
                 </li>
               </ul>
@@ -243,10 +274,10 @@ const Landing = ({ data }) => {
                 <li>
                   <h3>
                     The sheets are dried then mixed with carbon black, synthetic
-                    rubber and reinforced with Steel wire to produce the tires
-                    of today.
+                    rubber and reinforced with Steel wire to produce the tyres
+                    of today. Average life of a tractor tyre is 5 years.
                   </h3>
-                  <h3>Average life of a tractor tire is 5 years</h3>
+                  <h3>Average life of a tractor tyre is 5 years.</h3>
                 </li>
               </ul>
             </div>
@@ -268,7 +299,7 @@ const Landing = ({ data }) => {
             <div className="col-md-5">
               <ul className="features">
                 <li>
-                  <h3>With your help the rubber can have one final use</h3>
+                  <h3>With your help the rubber can have one final use.</h3>
                   <h3>
                     To make up the surface, tyres are processed into small
                     rubber fragments and mixed with sand to form a soft, grippy
